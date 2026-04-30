b1286f54f963d8e1577f9f8df2de31fa0f1b2d7f
MATRUNI <h.uyadav2004@gmail.com>
2026-05-01 01:18:50 +0530

    Feat: added CORS middleware

--- my_services/src/app.js
+++ my_services/src/app.js
@@ -1,7 +1,8 @@
 import express from'express';
 import router from './routes/productRoutes.js';
+import cors from 'cors'
 const app=express();
-
+app.use(cors())
 app.use(express.json());
 
 // Printing the route and method server is recieving

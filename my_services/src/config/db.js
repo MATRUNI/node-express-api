cbeb6f5bc9cc12f71837f2c2f286768bbd8608e8
MATRUNI <h.uyadav2004@gmail.com>
2026-05-23 00:38:43 +0530

    fix: node and npm versions

--- my_services/src/config/db.js
+++ my_services/src/config/db.js
@@ -4,7 +4,7 @@ const connectDB=async ()=>
 {
     try 
     {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI+"test")
         console.log("Database Connected");
     } catch (error) 
     {

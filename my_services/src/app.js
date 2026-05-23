d115ed1cbfef16fa3ed04c2329edf0071a063cf2
MATRUNI <h.uyadav2004@gmail.com>
2026-05-24 01:36:58 +0530

    feat: add /health route and enhance request logger with timestamps

--- my_services/src/app.js
+++ my_services/src/app.js
@@ -8,9 +8,13 @@ app.use(express.json());
 
 // Printing the route and method server is recieving
 app.use((req,res,next)=>{
-    console.log("REQUEST:",req.method,req.url);
+    console.log(`REQUEST: [${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
 });
+// for waking the cold server
+app.get('/health',(req,res)=>{
+    res.json({health:"OK"})
+})
 // app.use('/products',router);
 app.use('/api',APIRouter)
 export default app;

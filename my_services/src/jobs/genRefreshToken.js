671ea12b94731ea5d3551e46c4b8bb0a03bc398d
MATRUNI <h.uyadav2004@gmail.com>
2026-06-07 00:02:13 +0530

    feat: dynamic partitioned

--- my_services/src/jobs/genRefreshToken.js
+++ my_services/src/jobs/genRefreshToken.js
@@ -2,13 +2,13 @@ import jwt from 'jsonwebtoken'
 
 export default function genRefreshToken(payload,res)
 {
-    const token=jwt.sign(payload, process.env.SECRET_KEY, {expiresIn:'30d'});
+    const token=jwt.sign({...payload, type:"user"}, process.env.REFRESH_SECRET_KEY, {expiresIn:'30d'});
     const isProd = process.env.NODE_ENV === "production"
     res.cookie('refresh_token',token,{
         httpOnly:true,
         secure: isProd,
         sameSite: isProd?"none":"lax",
-        partitioned: true,
+        partitioned: isProd,
         maxAge: 30*24*60*60*1000
     })
     console.log('Access token dipatched')

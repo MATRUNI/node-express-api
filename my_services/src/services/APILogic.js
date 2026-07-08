2c657d1e424b69a4fedc08c986480c6391b302db
MATRUNI <h.uyadav2004@gmail.com>
2026-07-08 21:18:31 +0530

    fix: sort api List accordingly if has config or not

--- my_services/src/services/APILogic.js
+++ my_services/src/services/APILogic.js
@@ -6,7 +6,7 @@ export async function getAPIs(user)
 {
     const isLoggedIn = !!user;
     const filter = isLoggedIn? {} : {priority: 0};
-    return await API.find(filter).sort({ priority: -1, createdAt: -1 });
+    return await API.find(filter).sort({ priority: -1, hasConfig:-1 });
 }
 export async function createNewApi(apiInstance) 
 {

af6fe498d83583dc9b6ae723b261bd9e0eceba2c
MATRUNI <h.uyadav2004@gmail.com>
2026-07-26 00:14:13 +0530

    feat: support one-to-many and many-to-many calls

--- my_services/src/socket/handlers.js
+++ my_services/src/socket/handlers.js
@@ -1,39 +1,155 @@
 
 export default function registerSocketHandler(io)
 {
-    console.log('connecting')
+    const calls = new Map();
     io.on("connection",(socket)=>{
-        const userId = socket.user.userId
-        
-        socket.join(`user:${userId}`);
-        
+        const username = socket.user.username
+
+        // user's room
+        socket.join(`user:${username}`);
+
+        // updating UI with latest people only
         sendOnlineCount(io)
 
+        // listens to chat:send from users and broadCast to others
         socket.on("chat:send",(data)=>{
             socket.broadcast.emit("chat:receive",data)
         })
-        socket.on("offer",(data)=>{
-            socket.broadcast.emit("offer",data)
-        })
-        socket.on("answer",(answer)=>{
-            socket.broadcast.emit("answer",answer)
-        })
-        socket.on("ice",(candidate)=>{
-            socket.broadcast.emit("ice",candidate)
-        })
-        socket.on("call:end",()=>{
-            socket.broadcast.emit("call:end")
+        
+        // creating call from frontend
+        socket.on("call:create",()=>{
+            const callId = crypto.randomUUID();
+
+            calls.set(callId, {
+                host:username,
+                participants:new Set([username])
+            });
+
+            socket.join(`call:${callId}`);
+
+            socket.data.callId = callId;
+
+            socket.emit("call:created",{callId})
         })
 
-        socket.on("disconnect",()=>{
-            sendOnlineCount(io)
+        // invite people in the calls
+        socket.on('call:invite',({to,callId})=>{
+            const call = calls.get(callId)
+            if(!call) return;
+
+            io.to(`user:${to}`).emit("call:invite",{
+                from:username,
+                callId
+            })
+        });
+
+        // invited people joining the call room
+        socket.on("call:join",({callId})=>{
+            const call = calls.get(callId);
+            if(!call) return;
+
+            const existingUsers = [...call.participants];
+
+            call.participants.add(username);
+            socket.join(`call:${callId}`);
+
+            socket.data.callId = callId;
+
+            socket.emit("call:joined",{existingUsers})
+
+            io.to(`call:${callId}`).emit('call:update',{
+                participants:[...call.participants]
+            })
         })
+        socket.on("users:online:get", () => {
+            io.emit("users:online", getOnlineUsers(io));
+        });
+        // making offer
+        socket.on("offer",({to,offer})=>{
+            socket.to(`user:${to}`).emit("offer",{from:username,offer})
+        })
+
+        // answer to the offer
+        socket.on('answer',({to,answer})=>{
+            socket.to(`user:${to}`).emit("answer",{from:username,answer})
+        })
+
+        // ICE: Interactive Connectivity Establishment.
+        // Find a network path so two peers can communicate directly.
+        socket.on("ice",({to,candidate})=>{
+            socket.to(`user:${to}`).emit("ice",{from:username,candidate})
+        })
+
+        // user leaves a call
+        socket.on("call:end", () => {
+            const callId = socket.data.callId;
+            const call = calls.get(callId);
+        
+            if (!call) return;
+            
+            call.participants.delete(username);
+
+            if(call.host === username){
+                call.host = [...call.participants][0] || null;
+            }
+
+            socket.leave(`call:${callId}`);
+            socket.data.callId = null;
+
+            io.to(`call:${callId}`).emit("peer:left", {
+                peer: username,
+                reason:"left"
+            });
+
+            io.to(`call:${callId}`).emit("call:update", {
+                participants:[...call.participants]
+            });
+
+            if (call.participants.size === 0) {
+                calls.delete(callId);
+            }
+        });
+
+        // getting kicked of the call
+        socket.on("call:kick", ({callId, peer}) => {
+
+            const call = calls.get(callId);
+
+            if (!call) return;
+
+            if (call.host !== username)
+            return;
+
+            if(!call.participants.has(peer))
+            return;
+
+            call.participants.delete(peer);
+            
+            const kickedSockets = io.sockets.adapter.rooms.get(`user:${peer}`);
+
+            kickedSockets?.forEach(socketId => {
+                const kicked = io.sockets.sockets.get(socketId)
+                kicked?.leave(`call:${callId}`);
+            });
+
+            io.to(`call:${callId}`).emit("peer:left", {
+                peer,
+                reason:"kicked"
+            });
+        
+            io.to(`call:${callId}`).emit("call:update", {
+                participants:[
+                    ...call.participants
+                ]
+            });
+        });
     })
 }
+function getOnlineUsers(io) {
+    return [...io.sockets.adapter.rooms.keys()]
+        .filter(room => room.startsWith("user:")).map(room => room.substring(5));;
+}
 function sendOnlineCount(io) {
-    const onlineUsers = [...io.sockets.adapter.rooms.keys()]
-        .filter(room => room.startsWith("user:"))
-        .length;
-
-    io.emit("members", onlineUsers);
+    const onlineUsers = getOnlineUsers(io);
+    io.emit("members", onlineUsers.length);
 }

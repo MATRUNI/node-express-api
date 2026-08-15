672609eab46b309a2adbbd3e55b8b8a47e72bec3
MATRUNI <h.uyadav2004@gmail.com>
2026-08-15 19:59:15 +0530

    fix(core): make AppErrors operational to prevent generic 500 crashes

--- my_services/src/utils/AppErrors.js
+++ my_services/src/utils/AppErrors.js
@@ -6,6 +6,7 @@ export default class AppErrors extends Error
         this.statusCode=statusCode;
         this.status=`${statusCode}`.startsWith('4')?"fail":"error";
         this.errors=errors;
+        this.isOperational = true;
         Error.captureStackTrace(this,this.constructor)
     }
 }

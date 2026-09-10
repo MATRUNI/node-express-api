5528350b4fc26ebe55a1ab81e8513bb305803a9e
MATRUNI <h.uyadav2004@gmail.com>
2026-09-10 19:13:43 +0530

    feat: add API config creation logic

--- my_services/src/controller/configController.js
+++ my_services/src/controller/configController.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import APIConfig from "../models/API_Config.js"
+import API from "../models/API.js";
 
 export const getById = async (id)=>{
     if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -13,4 +14,29 @@ export const getById = async (id)=>{
     }
 
     return { success: true, data };
+}
+
+export const setAPIConfig = async (config,id)=>{
+    const {title} = config
+    const apiconfog = new APIConfig(config)
+
+    try {
+        const [data] = await Promise.all([
+            API.findById(id),
+            apiconfog.validate()
+        ])
+        
+        if( !data || !title.includes(data.name))
+        {
+            return {success:false,message:"API doesn't exist"}
+        }
+
+        const [creation,updation] = await Promise.all([
+            APIConfig.create({_id:id,...config}),
+            API.updateOne({_id:id},{$set:{hasConfig:true}})
+        ]);
+        return {success:true,data:creation}
+    } catch (error) {
+        return {success:false,message:"Validation error",error}
+    }
 }
